d29b6fc69c7467d02346a20743c0b248be804ea5
Philip Giuliani <info@philipg.net>
2015-09-18 13:31:35 +0200

    Start using database/sql

--- handlers.go
+++ handlers.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func LibrariesHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	libraries := Libraries{
-		Library{ID: 1, Name: "Bolzano", TakenPlaces: 5, TotalPlaces: 180},
-		Library{ID: 2, Name: "Bologna", TakenPlaces: 99, TotalPlaces: 123},
-	}
+func LibrariesHandler(db *sql.DB) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		rows, err := db.Query("SELECT id, name, taken_places, total_places FROM libraries")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
 
-	json.NewEncoder(w).Encode(libraries)
+		libraries := Libraries{}
+		for rows.Next() {
+			library := Library{}
+			err = rows.Scan(&library.ID, &library.Name, &library.TakenPlaces, &library.TotalPlaces)
+			if err != nil {
+				log.Fatal(err)
+			}
+			libraries = append(libraries, library)
+		}
+
+		json.NewEncoder(w).Encode(libraries)
+	}
 }
 
 func LibraryHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+)
 
-	"github.com/julienschmidt/httprouter"
+const (
+	dbUser     = "philipgiuliani"
+	dbPassword = ""
+	dbName     = "philipgiuliani"
 )
 
 func main() {
-	router := httprouter.New()
-    router.GET("/libraries", LibrariesHandler)
-    router.GET("/libraries/:libraryID", LibraryHandler)
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		dbUser, dbPassword, dbName)
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+	router := httprouter.New()
+	router.GET("/libraries", LibrariesHandler(db))
+	router.GET("/libraries/:libraryID", LibraryHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
