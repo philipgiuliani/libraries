0c2b397ceafe03cf685a93d8fd0e7231fdd56eed
Philip Giuliani <info@philipg.net>
2015-09-18 14:21:48 +0200

    Use autoload

--- main.go
+++ main.go
@@ -3,21 +3,15 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/julienschmidt/httprouter"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
-	"github.com/joho/godotenv"
 	"os"
 )
 
 func main() {
-	// load .env files
-	envErr := godotenv.Load()
-	if envErr != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	// connect to the database
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
