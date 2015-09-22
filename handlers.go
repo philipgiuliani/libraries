06d54ecaaa6c67bb49687f4dec1ce3c279377685
Philip Giuliani <info@philipg.net>
2015-09-22 20:51:40 +0200

    Add detailed fields

--- handlers.go
+++ handlers.go
@@ -6,11 +6,33 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func LibrariesHandler(db *sql.DB) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		rows, err := db.Query("SELECT id, name, taken_places, total_places FROM libraries")
+		queryParams := r.URL.Query()
+
+		// Parse location data
+		var latitude float64
+		var longitude float64
+		if queryParams.Get("latitude") != "" && queryParams.Get("longitude") != "" {
+			latitude, _ = strconv.ParseFloat(queryParams.Get("latitude"), 32)
+			longitude, _ = strconv.ParseFloat(queryParams.Get("longitude"), 32)
+		}
+
+		var rows *sql.Rows
+		var err error
+		if latitude != 0 && longitude != 0 {
+			rows, err = db.Query(`
+					SELECT id, name, taken_places, total_places, earth_distance(ll_to_earth(latitude, longitude), ll_to_earth($1, $2)) AS distance
+					FROM libraries
+					ORDER BY distance ASC`,
+				latitude, longitude)
+		} else {
+			rows, err = db.Query("SELECT id, name, taken_places, total_places FROM libraries")
+		}
+
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -19,7 +41,7 @@ func LibrariesHandler(db *sql.DB) httprouter.Handle {
 		libraries := Libraries{}
 		for rows.Next() {
 			library := Library{}
-			err = rows.Scan(&library.ID, &library.Name, &library.TakenPlaces, &library.TotalPlaces)
+			err := rows.Scan(&library.ID, &library.Name, &library.TakenPlaces, &library.TotalPlaces, &library.Distance)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -35,8 +57,8 @@ func LibraryHandler(db *sql.DB) httprouter.Handle {
 		libraryID := ps.ByName("id")
 
 		library := Library{}
-		err := db.QueryRow("SELECT id, name, taken_places, total_places FROM libraries WHERE id = $1", libraryID).
-			Scan(&library.ID, &library.Name, &library.TakenPlaces, &library.TotalPlaces)
+		err := db.QueryRow("SELECT id, name, taken_places, total_places, latitude, longitude, description, city, country_code, contact FROM libraries WHERE id = $1", libraryID).
+			Scan(&library.ID, &library.Name, &library.TakenPlaces, &library.TotalPlaces, &library.Latitude, &library.Longitude, &library.Description, &library.City, &library.CountryCode, &library.Contact)
 
 		switch {
 		case err == sql.ErrNoRows:
--- library.go
+++ library.go
@@ -1,11 +1,18 @@
 package main
 
 type Library struct {
-	ID          uint   `json:"id"`
-	MappingID   string `json:"-"`
-	Name        string `json:"name"`
-	TakenPlaces uint16 `json:"takenPlaces,omitempty"`
-	TotalPlaces uint16 `json:"totalPlaces,omitempty"`
+	ID          uint    `json:"id"`
+	MappingID   string  `json:"-"`
+	Name        string  `json:"name"`
+	TakenPlaces uint16  `json:"takenPlaces,omitempty"`
+	TotalPlaces uint16  `json:"totalPlaces,omitempty"`
+	Distance    float32 `json:"distance,omitempty"`
+	Latitude	float32 `json:"latitude,omitempty"`
+	Longitude	float32 `json:"longitude,omitempty"`
+	Description	string  `json:"description,omitempty"`
+	City		string  `json:"city,omitempty"`
+	CountryCode	string  `json:"countryCode,omitempty"`
+	Contact		string  `json:"contact,omitempty"`
 }
 
 type Libraries []Library
